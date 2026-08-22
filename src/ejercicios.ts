f32152dad23e974d9096c01d2b0488542f3a058a
mariajosehincapie18 <mjhincapie18@gmail.com>
2026-08-21 22:10:02 -0500

    ejercios 3 y 4

--- src/ejercicios.ts
+++ src/ejercicios.ts
@@ -9,3 +9,22 @@ for(let i=0; i < estudiantes.length; i++){
 //2. Imprimir cuantos estudiantes hay en el arreglo.
 
 console.log(estudiantes.length);
+
+
+// 3.Crear un array de números y calcular la suma total
+let num = [10, 20, 30, 40, 50];
+
+let suma = 0;
+for (let i = 0; i < num.length; i++) {
+    suma = suma + num[i];}
+
+console.log(suma); 
+
+4//Crear un array de números (mínimo 15) y calcular la suma total
+let num2 = [10, 20, 30, 40, 50,10, 20, 30, 40, 50,10, 20, 30, 40, 50];
+
+let suma2 = 0;
+for (let i = 0; i < num2.length; i++) {
+    suma2 = suma2 + num2[i];}
+
+console.log(suma2); 
