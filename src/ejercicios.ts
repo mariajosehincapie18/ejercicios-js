69ef4a2bf18891307e6275d70ffceb78a814db7b
mariajosehincapie18 <mjhincapie18@gmail.com>
2026-08-21 21:52:10 -0500

    ejercicio 1 y 2

---
+++ src/ejercicios.ts
@@ -0,0 +1,11 @@
+//1. Crear un array con 5 nombres de estudiantes y 
+// mostrar cada nombre en consola usando un ciclo.
+
+let estudiantes = ["maria","juan","laura","carlos","ana"];
+for(let i=0; i < estudiantes.length; i++){
+    console.log(estudiantes[i]);
+}
+
+//2. Imprimir cuantos estudiantes hay en el arreglo.
+
+console.log(estudiantes.length);
